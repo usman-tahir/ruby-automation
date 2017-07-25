5020568597820f0709b4636f1c2f64cadc24e368
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-25 09:56:45 -0400

    added a setup environment for conducting automation with Cucumber, Selenium WebDriver, and Ruby

---
+++ access-google-2/env.rb
@@ -0,0 +1,19 @@
+require 'selenium/webdriver'
+
+url = "http://#{ENV['TB_KEY']}:#{ENV['TB_SECRET']}@hub.testingbot.com/wd/hub"
+
+capabilities = Selenium::WebDriver::Remote::Capabilities.new
+capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'ANY'
+capabilities['name'] = 'My first test'
+capabilities['browserName'] = ENV['SELENIUM_BROWSER'] || 'chrome'
+capabilities['version'] = ENV['SELENIUM_VERSION'] if ENV['SELENIUM_VERSION']
+
+browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
+
+Before do |scenario|
+    @browser = browser
+end
+
+at_exit do
+    browser.quit
+end
